51f6fad2d9d4425c11fbd0e31c4e4a6b05e994c1
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-04-04 16:48:58 +0700

    fix entity & gorm properties

--- entity/parent.go
+++ entity/parent.go
@@ -1,14 +1,14 @@
 package entity
 
 type ParentEntity struct {
-	ParentId int            `gorm:"column:parent_id;autoIncrement;primaryKey;type:int"`
-	Username string         `gorm:"column:username;index:username_unique,unique;type:varchar(50)"`
-	Email    string         `gorm:"column:email;type:varchar(100)"`
-	Password string         `gorm:"column:password;type:varchar(50)"`
-	Gender   string         `gorm:"column:gender;type:varchar(10)"`
-	Child    *StudentEntity `gorm:"association_foreignKey:StudentId"`
-	ChildId  int            `gorm:"column:child_id;type:int"`
-	Avatar   string         `gorm:"column:avatar;type:mediumtext"`
+	ParentId  int            `gorm:"column:parent_id;autoIncrement;primaryKey;type:int"`
+	Username  string         `gorm:"column:username;index:username_unique,unique;type:varchar(50)"`
+	Email     string         `gorm:"column:email;type:varchar(100)"`
+	Password  string         `gorm:"column:password;type:varchar(50)"`
+	Gender    string         `gorm:"column:gender;type:varchar(10)"`
+	Student   *StudentEntity `gorm:"association_foreignKey:StudentId"`
+	StudentId int            `gorm:"column:student_id;type:int"`
+	Avatar    string         `gorm:"column:avatar;type:mediumtext"`
 }
 
 func (ParentEntity) TableName() string {
