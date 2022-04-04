de4f54cc1c410445ace435217f08ce8ef6aaf683
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-04-04 15:47:53 +0700

    fix entity & gorm properties

--- entity/admin.go
+++ entity/admin.go
@@ -5,6 +5,7 @@ type AdminEntity struct {
 	Username string `gorm:"column:username;unique;type:varchar(50)"`
 	Email    string `gorm:"column:email;type:varchar(100)"`
 	Password string `gorm:"column:password;type:varchar(100)"`
+	Gender   string `gorm:"column:gender;type:varchar(10)"`
 	Avatar   string `gorm:"column:avatar;type:mediumtext"`
 }
 
--- entity/transaction.go
+++ entity/transaction.go
@@ -7,7 +7,7 @@ type Transaction struct {
 	Child           *StudentEntity `gorm:"association_foreignKey:StudentId"`
 	ChildId         int            `gorm:"column:child_id;type:int"`
 	TotalAmount     float64        `gorm:"column:total_amount;type:double"`
-	Subscription    int            `gorm:"column:subcription;type:int"`
+	Subscription    int            `gorm:"column:subscription;type:bool"`
 	TransactionDate time.Time      `gorm:"column:transaction_date"`
 	Status          bool           `gorm:"column:status;type:bool"`
 }
