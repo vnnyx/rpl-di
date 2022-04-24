291617625476f984e1957184dd752dda985be625
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-04-25 05:44:08 +0700

    fix entity, add service & model

--- sixmath-app/entity/user.go
+++ sixmath-app/entity/user.go
@@ -4,6 +4,7 @@ type UserEntity struct {
 	UserId          int         `gorm:"column:user_id;primaryKey;autoIncrement"`
 	Username        string      `gorm:"column:username;unique;type:varchar(50)"`
 	Email           string      `gorm:"column:email;type:varchar(100)"`
+	Handphone       string      `gorm:"column:handphone;type:varchar(20)"`
 	Password        string      `gorm:"column:password;type:varchar(50)"`
 	Role            string      `gorm:"column:role;type:varchar(10)"`
 	Gender          string      `gorm:"column:gender;type:varchar(10)"`
