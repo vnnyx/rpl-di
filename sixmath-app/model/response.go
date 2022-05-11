62040e1340bba7672d3740813c45f188327057d6
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-11 13:40:02 +0700

    remove unnecessary file

--- sixmath-app/model/response.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Response struct {
-	Code   int         `json:"code,omitempty"`
-	Status string      `json:"status,omitempty"`
-	Data   interface{} `json:"data,omitempty"`
-	Error  interface{} `json:"error,omitempty"`
-}
