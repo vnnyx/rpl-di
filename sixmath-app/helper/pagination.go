8ad9aa57c20e77aa66f8eeee9f5bcb8c6bea08a8
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-13 19:19:44 +0700

    add pagination

---
+++ sixmath-app/helper/pagination.go
@@ -0,0 +1,20 @@
+package helper
+
+import (
+	"gorm.io/gorm"
+	"math"
+	"rpl-sixmath/model"
+)
+
+func Paginate(value interface{}, pagination *model.Pagination, db *gorm.DB) func(db *gorm.DB) *gorm.DB {
+	var totalRows int64
+	db.Model(value).Count(&totalRows)
+
+	pagination.TotalRows = totalRows
+	totalPages := int(math.Ceil(float64(totalRows) / float64(pagination.Limit)))
+	pagination.TotalPages = totalPages
+
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort())
+	}
+}
