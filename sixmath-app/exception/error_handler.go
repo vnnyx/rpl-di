9e7ec21175ba37ed77aa91e1b557e3591e8d1a3f
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-13 19:19:28 +0700

    fix error handler

--- sixmath-app/exception/error_handler.go
+++ sixmath-app/exception/error_handler.go
@@ -1,6 +1,7 @@
 package exception
 
 import (
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"rpl-sixmath/model"
 )
@@ -8,10 +9,34 @@ import (
 func ErrorHandler(ctx *fiber.Ctx, err error) error {
 	_, ok := err.(ValidationError)
 	if ok {
+		var obj interface{}
+		_ = json.Unmarshal([]byte(err.Error()), &obj)
 		return ctx.Status(fiber.StatusBadRequest).JSON(model.WebResponse{
 			Code:   400,
 			Status: "BAD_REQUEST",
-			Data:   err.Error(),
+			Data:   struct{}{},
+			Error:  obj,
+		})
+	}
+	if err.Error() == "USERNAME_REGISTERED" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.Response{
+			Code:   fiber.StatusBadRequest,
+			Status: "BAD_REQUEST",
+			Data:   nil,
+			Error: map[string]interface{}{
+				"username": "MUST_UNIQUE",
+			},
+		})
+	}
+
+	if err.Error() == "PLAYLIST_NOT_FOUND" {
+		return ctx.Status(fiber.StatusNotFound).JSON(model.Response{
+			Code:   fiber.StatusNotFound,
+			Status: "BAD_REQUEST",
+			Data:   nil,
+			Error: map[string]interface{}{
+				"playlist_id": "NOT_FOUND",
+			},
 		})
 	}
 	return ctx.Status(fiber.StatusInternalServerError).JSON(model.WebResponse{
