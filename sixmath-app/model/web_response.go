71c3a84bed5d3a158d15648680192ccd22e32bbd
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-13 19:22:28 +0700

    refactor: change response model

--- sixmath-app/model/web_response.go
+++ sixmath-app/model/web_response.go
@@ -4,4 +4,5 @@ type WebResponse struct {
 	Code   int         `json:"code,omitempty"`
 	Status string      `json:"status,omitempty"`
 	Data   interface{} `json:"data,omitempty"`
+	Error  interface{} `json:"errors"`
 }
