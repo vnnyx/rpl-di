7079fbc63d14c693403ec256acf6a7a0bd3161e8
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-18 11:55:02 +0700

    fix: environment variable

--- sixmath-app/backend/main.go
+++ sixmath-app/backend/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	configuration := config.New()
+	configuration := config.New(".env")
 	databases := config.NewMySQLDatabase(configuration)
 	migration.MyMigration(databases,
 		entity.User{},
