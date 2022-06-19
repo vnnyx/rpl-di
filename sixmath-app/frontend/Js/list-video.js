ae50c75ffaa4f58eac59066389ad33c6244ca687
yogifirgiawan <yogifirgiawan10@gmail.com>
2022-06-19 22:23:55 +0700

    tes

---
+++ sixmath-app/frontend/Js/list-video.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    let sidebar = document.querySelector(".sidebar")
+    $('#btn').on('click', function () {
+        sidebar.classList.toggle("act");
+    });
+    $('li').on('click', function () {
+        $(this).siblings().removeClass('akt')
+        $(this).addClass('akt')
+        console.log('ok')
+    });
+});
