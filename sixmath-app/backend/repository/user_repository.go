8c6f1ac9c62cea36efcbed84805085b9f43f4538
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-20 19:37:17 +0700

    feat: find user by email

--- sixmath-app/backend/repository/user_repository.go
+++ sixmath-app/backend/repository/user_repository.go
@@ -10,6 +10,7 @@ type UserRepository interface {
 	UpdateUser(user entity.User) (entity.User, error)
 	DeleteUser(userId int) error
 	FindUserById(userId int) (response entity.User, err error)
+	FindUserByEmail(email string) (response entity.User, err error)
 	FindUserByUsername(username string) (response entity.User, err error)
 	FindAllTeacher() (response []entity.User, err error)
 	FindAll() (response []entity.User, err error)
