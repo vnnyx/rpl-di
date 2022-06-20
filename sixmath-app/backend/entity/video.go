a3244f4b496cf1a292619c5cbe971f1091eb54f2
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-20 08:03:06 +0700

    fix: relationship

--- sixmath-app/backend/entity/video.go
+++ sixmath-app/backend/entity/video.go
@@ -1,11 +1,12 @@
 package entity
 
 type Video struct {
-	VideoId     int    `gorm:"column:video_id;primaryKey;autoIncrement;type:int" json:"video_id"`
-	Teacher     string `gorm:"teacher;type:varchar(100)" json:"teacher"`
-	Title       string `gorm:"column:title;type:varchar(200)" json:"title"`
-	URLVideo    string `gorm:"column:url_video;type:varchar(200)" json:"url_video"`
-	Description string `gorm:"column:description;type:longtext" json:"description"`
+	VideoId         int    `gorm:"column:video_id;primaryKey;autoIncrement;type:int" json:"video_id"`
+	TeacherUsername string `gorm:"column:teacher;type:varchar(100)" json:"teacher"`
+	Teacher         *User  `gorm:"association_foreignkey:TeacherUsername;references:Username" json:"-"`
+	Title           string `gorm:"column:title;type:varchar(200)" json:"title"`
+	URLVideo        string `gorm:"column:url_video;type:varchar(200)" json:"url_video"`
+	Description     string `gorm:"column:description;type:longtext" json:"description"`
 }
 
 func (Video) TableName() string {
